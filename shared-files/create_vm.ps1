9f7a92bea7bc4d286235f7e39135eb9d1dcbc448
root <root@ansible.ilba.cat>
2020-12-18 18:27:02 +0100

    Crear equipo en Hyper-V.... 1

--- shared-files/create_vm.ps1
+++ shared-files/create_vm.ps1
@@ -5,7 +5,7 @@ $RAM = $args[2]
 new-item "D:\$VM" -itemtype directory
 new-item "D:\$VM\Virtual Hard Disks" -itemtype directory
 $Path = "D:\" + $VM
-$Source = 'E:\ExportVM\template_Ubuntu18.04LTS\Virtual Machines\F28E59A8-8CC4-4FE4-80FD-120F07F746F4.vmcx'
+$Source = 'E:\ExportVM\template_Ubuntu20.04LTS\Virtual Machines\F28E59A8-8CC4-4FE4-80FD-120F07F746F4.vmcx'
 $Import = Import-VM -Path $Source -Copy -GenerateNewId -VirtualMachinePath $Path -VhdDestinationPath "D:\$VM\Virtual Hard Disks" -SnapshotFilePath $Path
 Foreach ($Import in $Import)
 {
